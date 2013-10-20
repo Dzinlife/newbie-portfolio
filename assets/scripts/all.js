cb4145f262e559aee092ce1a56c5a0567f80bfcf
Dzinlife <dzinlife@me.com>
2013-10-20 16:25:50 +0800

    bugfix

--- assets/scripts/all.js
+++ assets/scripts/all.js
@@ -602,7 +602,7 @@ $(document).ready(function () {
 				"transform": "perspective(200) translateZ(0)",
 				"opacity": "1"
 			});
-			$("#skills-detail-container").removeAttr("style")；
+			$("#skills-detail-container").removeAttr("style");
 			$("#skills-detail-container").css("opacity","0");	
 			skillOpened = null;
 		}
